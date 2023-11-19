750328741d7d27248f2a988c99ad19730f316962
dosmasmexican <145898871+dosmasmexican@users.noreply.github.com>
2023-11-20 02:42:20 +0900

    repeller 객체 생성
    repeller 객체 생성

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let emitters = [];
+let repellers = [];
 
 function setup() {
   createCanvas(640, 240);
@@ -11,11 +12,16 @@ function draw() {
     let gravity = createVector(0, 0.1);
     emitter.applyForce(gravity);
     emitter.addParticle();
+    for (let repeller of repellers) {
+      repeller.show();
+    }
   }
 
   function mousePressed() {
     if (mouseButton === LEFT) {
       emitters.push(new Emitter(mouseX, mouseY));
+    } else if (mouseButton === RIGHT) {
+      repellers.push(new Repeller(mouseX, mouseY));
     }
   }
 }
