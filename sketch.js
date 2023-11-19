05c00c66baadbcf2c181a09b2a27a0914811ba1f
dosmasmexican <145898871+dosmasmexican@users.noreply.github.com>
2023-11-20 02:39:50 +0900

    emitter 객체 생성
    emitter 객체 생성

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let emitters = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 240);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  for (let emitter of emitters) {
+    emitter.run();
+    let gravity = createVector(0, 0.1);
+    emitter.applyForce(gravity);
+    emitter.addParticle();
+  }
+
+  function mousePressed() {
+    if (mouseButton === LEFT) {
+      emitters.push(new Emitter(mouseX, mouseY));
+    }
+  }
 }
